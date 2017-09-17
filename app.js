33661d888671bf7625667e4c8bb02dcf69079dc2
philipwisner <philipwisner@gmail.com>
2017-09-18 00:37:59 +0200

    app config and flow

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const configPassport = require('./middleware/passport');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+//MODELS
+const User = require('./models/user');
 
-var app = express();
+//MONGOOSE CONNECTION
+mongoose.connect('mongodb://localhost:27017/farm-voyage-development');
+
+//DEFINE EXPRESS APP
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('layout', 'layouts/main-layout');
+app.use(expressLayouts);
+
+
+app.use(session({
+  secret: 'farm-voyage-dev',
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore( { mongooseConnection: mongoose.connection })
+}))
+
+configPassport(passport);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,12 +49,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//PASSPORT
+app.use(flash());
+
+//ROUTES
+const index = require('./routes/index');
+// const auth = require('./routes/auth');
+// const profile = require('./routes/profile');
+// const users = require('./routes/users');
+
 app.use('/', index);
-app.use('/users', users);
+// app.use('/', auth);
+// app.use('/profile', profile);
+// app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
---
+++ middleware/passport.js
@@ -0,0 +1,142 @@
+// config/passport.js
+
+// load all the things we need
+const LocalStrategy = require('passport-local').Strategy;
+const FbStrategy = require('passport-facebook').Strategy;
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+
+const passport = require("passport");
+
+const bcrypt = require('bcrypt');
+const bcryptSalt     = 10;
+
+const User = require("../models/user");
+
+// load up the user model
+
+
+// expose this function to our app using module.exports
+module.exports = (passport) => {
+
+    // =========================================================================
+    // passport session setup ==================================================
+    // =========================================================================
+    // required for persistent login sessions
+    // passport needs ability to serialize and unserialize users out of session
+
+    // used to serialize the user for the session
+    passport.serializeUser((user, cb) => {
+        cb(null, user.id);
+    });
+
+    passport.deserializeUser((id, cb) => {
+    User.findById(id, (err, user) => {
+        if (err) { return cb(err); }
+        cb(null, user);
+    });
+    });
+
+    passport.use('local-login', new LocalStrategy((username, password, next) => {
+        User.findOne({ username }, (err, user) => {
+            if (err) {
+            return next(err);
+            }
+            if (!user) {
+            return next(null, false, { message: "Incorrect username" });
+            }
+            if (!bcrypt.compareSync(password, user.password)) {
+            return next(null, false, { message: "Incorrect password" });
+            }
+
+            return next(null, user);
+        });
+    }));
+
+    passport.use('local-signup', new LocalStrategy(
+    { passReqToCallback: true },
+    (req, username, password, next) => {
+        process.nextTick(() => {
+            User.findOne({
+                'username': username
+            }, (err, user) => {
+                if (err){ return next(err); }
+
+                if (user) {
+                    return next(null, false);
+                } else {
+                    const { username, email, description, password } = req.body;
+                    const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+                    const newUser = new User({
+                    username,
+                    email,
+                    description,
+                    password: hashPass
+                    });
+
+                    newUser.save((err) => {
+                        if (err){ next(err); }
+                        return next(null, newUser);
+                    });
+                }
+            });
+        });
+    }));
+
+    //FACEBOOK STRATEGY
+    passport.use(new FbStrategy({
+      clientID: "857307061101319",
+      clientSecret: "6a3eace08f83082fe80681f9078c0105",
+      callbackURL: "/auth/facebook/callback"
+    }, (accessToken, refreshToken, profile, done) => {
+      User.findOne({ facebookID: profile.id }, (err, user) => {
+        if (err) {
+          return done(err);
+        }
+        if (user) {
+          return done(null, user);
+        }
+
+        const newUser = new User({
+          facebookID: profile.id
+        });
+
+        newUser.save((err) => {
+          if (err) {
+            return done(err);
+          }
+          done(null, newUser);
+        });
+      });
+
+    }));
+
+
+    //GOOGLE STRATEGY
+    passport.use(new GoogleStrategy({
+      clientID: "881861196583-d8gjkkped1kgukjsg9cb6eedqja3k8c0.apps.googleusercontent.com",
+      clientSecret: "JwlD-62nOiJLnODXHrWR6ftV",
+      callbackURL: "/auth/google/callback"
+    }, (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleID: profile.id }, (err, user) => {
+        if (err) {
+          return done(err);
+        }
+        if (user) {
+          return done(null, user);
+        }
+
+        const newUser = new User({
+          googleID: profile.id
+        });
+
+        newUser.save((err) => {
+          if (err) {
+            return done(err);
+          }
+          done(null, newUser);
+        });
+      });
+
+    }));
+
+};
