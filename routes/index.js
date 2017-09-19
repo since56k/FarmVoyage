0ff383b99d986906bad7b62c00558898a89fdcf5
philipwisner <philipwisner@gmail.com>
2017-09-19 10:06:27 +0200

    main search page w/ route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+//get home page
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Farm Voyage' });
+});
+
+//home to main search
+router.get('/main', (req, res) => {
+    res.render('main', {
+    message: req.flash("error"),
+	});
 });
 
 module.exports = router;
