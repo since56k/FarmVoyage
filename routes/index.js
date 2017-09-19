ff3d584c899a079049047e11daf437353c04d5b3
philipwisner <philipwisner@gmail.com>
2017-09-19 09:39:02 +0200

    added main layout, index, signin/up

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Index' });
+  res.render('index', { title: 'Farm Voyage' });
 });
 
 module.exports = router;
