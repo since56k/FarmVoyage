831c5a4da4c3f0db5ee8ff58ac9e841382944037
philipwisner <philipwisner@gmail.com>
2017-09-19 16:05:37 +0200

    fixed redirect to home

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ const passport   = require('passport');
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
 //get home page
-router.get('/', function(req, res, next) {
+router.get('/', ensureLoggedOut('/main'), function(req, res, next) {
   res.render('index', { title: 'Farm Voyage' });
 });
 
 //home to main search
-router.get('/main', ensureLoggedIn('/login'), (req, res) => {
+router.get('/main', (req, res) => {
     res.render('main', {
     message: req.flash("error"),
     user : req.user
