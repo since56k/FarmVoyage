ce889c3e17df4b3694a12809745a05d61b03c0c1
lele <lele@crackberry.local>
2017-09-22 00:02:39 +0200

    add save place

--- routes/profile.js
+++ routes/profile.js
@@ -7,6 +7,7 @@ const User = require('../models/user.js');
 const ObjectId                = require('mongoose').Types.ObjectId;
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
+
 //index
 router.get('/', ensureLoggedIn('/login'), (req, res) => {
     res.render('profile/route', {
@@ -20,25 +21,21 @@ router.get('/route', ensureLoggedIn('/login'), (req, res) => {
     });
 });
 
-// router.get('/route/:id', ensureLoggedIn('/login'), (req, res, next) => {
-//   PlaceSaved.find({}).populate('userId').exec((err, places) => {
-//       res.render('profile/route', {palces});
-//     });
-  
-// });
-
-//display and crud for farm
-
 router.get('/farm', ensureLoggedIn('/login'), (req, res) => {
     res.render('profile/farm', {
         user : req.user
     });
 });
 
+router.get('/account', ensureLoggedIn('/login'), (req, res) => {
+    res.render('profile/account', {
+        user : req.user
+    });
+});
 
-//display and crud for user
-
-router.get('/account', ensureLoggedIn('/login'), (req, res, next) => {
+//Crud for User
+//show user
+router.get('/account', ensureLoggedIn(), (req, res, next) => {
   User.findById(req.user._id, (err, user) => {
     if (err) {
       return next(err);
@@ -48,8 +45,7 @@ router.get('/account', ensureLoggedIn('/login'), (req, res, next) => {
   });
 });
 
-
-//this for edit from a link
+//this for edit from a link 
 router.get('/account/:id', ensureLoggedIn(), (req, res, next) => {
   const userId = req.params.userId;
   if (req.user._id == userId) {
@@ -67,7 +63,7 @@ router.post('/account/:id', ensureLoggedIn(), (req, res, next) => {
       username: req.body.name,
       email: req.body.email
   };
-
+  
   User.findByIdAndUpdate(userId, updates, (err, user) => {
     if (err)       { return res.render('profile/account', { user, errors: user.errors }); }
     if (!user) { return next(new Error("404")); }
@@ -81,18 +77,14 @@ router.post('/account/:id', ensureLoggedIn(), (req, res, next) => {
 
 //Crud for Route
 // router.get('/:id/route', (req, res, next) => {
-//   PlaceSaved.findById(req.params.id, (err, route) => {
+//   PlaceSave.findById(req.params.id, (err, route) => {
 //     res.render('profile/route', { route })
 //   });
 // });
 
-// routes/index.js
-
-
-
 // router.post('/:id/delete', (req, res, next) => {
 //   const routeId = req.params.id;
-//   PlaceSaved.findByIdAndRemove(routeId, (err, route) => {
+//   PlaceSave.findByIdAndRemove(routeId, (err, route) => {
 //     if (err){ return next(err); }
 //     return res.redirect('profile/route');
 //   });
